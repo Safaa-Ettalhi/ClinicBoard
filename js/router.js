a69901ae018d0def584e25d68b338ca9424c7292
reda-elallaoui <elallaoui.reyad@gmail.com>
2025-09-19 23:05:28 +0100

    Ajout du système de routage - Navigation entre les pages de l'application

--- js/router.js
+++ js/router.js
@@ -1,14 +1,37 @@
-import { showLogin, showDashboard } from './pages.js';
+import { showLoginForm } from './login.js';
+import { showInitialSetup } from './init.js';
+import { showDashboard } from './dashboard.js';
+import { showPatients } from './patients.js';
+import { showAppointments } from './appointments.js';
+import { showFinances } from './finances.js';
+import { getData } from './storage.js';
 
 export function initRouter(){
   const root = document.getElementById('root');
 
   function renderPage(){
     const hash = location.hash.replace('#','');
-    if(hash === 'dashboard'){
-      showDashboard(root);
-    } else {
-      showLogin(root);
+    switch(hash){
+      case 'dashboard':
+        showDashboard(root);
+        break;
+      case 'patients':
+        showPatients(root);
+        break;
+      case 'appointments':
+        showAppointments(root);
+        break;
+      case 'finances':
+        showFinances(root);
+        break;
+      default:
+        document.body.className = 'with-sidebar';
+        const data = getData();
+        if(!data.auth?.passwordHash){
+          showInitialSetup(root);
+        } else {
+          showLoginForm(root);
+        }
     }
   }
 
